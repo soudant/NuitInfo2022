642a34af52f268b556712c0483a0f013554c7b72
Huseyin Yurtseven <yurtseven68210@hotmail.fr>
2022-12-01 21:51:06 +0100

    add component dall-e and loading Co-authored-by: Rémi Chavance <RemiChavance@users.noreply.github.com>

--- src/app/modules/dalle/dalle.module.ts
+++ src/app/modules/dalle/dalle.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DalleinputComponent } from './dalleinput/dalleinput.component';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { LoadingComponent } from './loading/loading.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+
 
 @NgModule({
-  declarations: [
-    DalleinputComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
-  exports: [
-    DalleinputComponent
-  ]
+  declarations: [DalleinputComponent, LoadingComponent],
+  imports: [CommonModule, FormsModule, MatInputModule, MatProgressSpinnerModule],
+  exports: [DalleinputComponent, LoadingComponent],
 })
-export class DalleModule { }
+export class DalleModule {}
--- src/app/modules/dalle/dalleinput/dalleinput.component.ts
+++ src/app/modules/dalle/dalleinput/dalleinput.component.ts
@@ -1,42 +1,40 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai';
 
 @Component({
   selector: 'app-dalleinput',
   templateUrl: './dalleinput.component.html',
-  styleUrls: ['./dalleinput.component.scss']
+  styleUrls: ['./dalleinput.component.scss'],
 })
 export class DalleinputComponent implements OnInit {
-
-  image = "";
-  openai: any
+  image = '';
+  openai: any;
   description!: string;
-  
-  public constructor() { }
+  isLoading = false;
+
+  public constructor() {}
 
   ngOnInit() {
     const configuration = new Configuration({
-      organization: "org-Lwaz9hCUPAkP0PmpdV5BruhG",
-      apiKey: "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",
+      organization: 'org-Lwaz9hCUPAkP0PmpdV5BruhG',
+      apiKey: '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',
     });
 
     this.openai = new OpenAIApi(configuration);
-
   }
 
   async onSubmitForm() {
     console.log(this.description);
-    
-    /*
+    //loading compnent here
+    this.isLoading = true;
+
     const response = await this.openai.createImage({
       prompt: this.description,
       n: 1,
-      size: "256x256",
+      size: '256x256',
     });
 
     this.image = response.data.data[0].url;
-    */
-    
-  } 
-
+    this.isLoading = false;
+  }
 }
