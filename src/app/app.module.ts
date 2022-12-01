5cd429affb2c611123a6c4cd976935508b0d7b00
dimitri1811 <96445988+dimitri1811@users.noreply.github.com>
2022-12-01 19:58:01 +0100

    services & formulaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormulaireComponent } from './module/formulaire/formulaire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormulaireComponent
   ],
   imports: [
     BrowserModule,
