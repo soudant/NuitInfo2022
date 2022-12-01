e41cda078e1da82f94ee9ff1226863a25c503bb2
Paul Delifer <paul.delifer@yahoo.fr>
2022-12-01 22:23:22 +0100

    s

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -6,12 +6,13 @@ import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import { HttpClientModule } from '@angular/common/http';
-
+import { MatCardModule} from '@angular/material/card';
 
 @NgModule({
     imports: [MatIconModule],
     exports: [
         MatIconModule,
+        MatCardModule,
         MatButtonModule,
         MatGridListModule,
         MatToolbarModule,
